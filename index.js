2bdaf1bff99bed0d1396616b6a574de0156048d6
phuongkon1998 <phuongkon1998@gmail.com>
2023-10-27 16:14:28 +0700

    test done

---
+++ index.js
@@ -0,0 +1,73 @@
+import React from "react";
+import "./style.css";
+
+export const Box = () => {
+  return (
+    <div className="box">
+      <div className="content">
+        <img className="logoncc" alt="Logoncc" src="logoncc-1.png" />
+        <div className="group">
+          <div className="overlap-group">
+            <p className="text-wrapper">Lorem ipsum dolor sit asmet?</p>
+            <p className="div">
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed tristique consequat placerat. Vestibulum
+              auctor pellentesque sem, eu posuere erat hendrerit quis. Maecenas vel consequat turpis. Nam facilisis,
+              ligula in mattis sodales, augue justo tristique nulla, sed lacinia ante eros ut mi. Morbi vitae diam
+              augue. Aliquam vel mauris a nibh auctor commodo. Praesent et nisi eu justo eleifend convallis. Quisque
+              suscipit maximus vestibulum. Phasellus congue mollis orci, sit amet luctus augue tristique eu. Donec
+              vulputate odio neque, sed semper turpis pellentesque a.
+            </p>
+          </div>
+        </div>
+        <div className="overlap-wrapper">
+          <div className="overlap">
+            <p className="p">
+              ullamcorper ultricies. Donec feugiat velit nulla, vel sodales est ullamcorper id. Aenean consequat
+              condimentum velit ut tempor. Nam porta massa in metus bibendum congue. Pellentesque ultrices vestibulum
+              mattis.
+            </p>
+            <p className="text-wrapper-2">
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed non dui sodales, faucibus libero ut, posuere
+              felis. Donec imperdiet suscipit accumsan. Aenean consequat condimentum velit ut tempor. Nam porta massa in
+              metus bibendum congue. Pellentesque ultrices liquam egestas nunc at
+            </p>
+            <p className="text-wrapper-3">Lorem ipsum dolor sit amet</p>
+            <img className="img" alt="Css icon" src="css-icon-1.png" />
+          </div>
+        </div>
+        <div className="overlap-group-wrapper">
+          <div className="overlap">
+            <p className="text-wrapper-3">Lorem ipsum dolor sit amet</p>
+            <img className="img" alt="Html icon" src="html-icon-1.png" />
+            <p className="p">
+              ullamcorper ultricies. Donec feugiat velit nulla, vel sodales est ullamcorper id. Aenean consequat
+              condimentum velit ut tempor. Nam porta massa in metus bibendum congue. Pellentesque ultrices vestibulum
+              mattis.
+            </p>
+            <p className="text-wrapper-2">
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed non dui sodales, faucibus libero ut, posuere
+              felis. Donec imperdiet suscipit accumsan. Aenean consequat condimentum velit ut tempor. Nam porta massa in
+              metus bibendum congue. Pellentesque ultrices liquam egestas nunc at
+            </p>
+          </div>
+        </div>
+        <div className="div-wrapper">
+          <div className="overlap">
+            <p className="p">
+              ullamcorper ultricies. Donec feugiat velit nulla, vel sodales est ullamcorper id. Aenean consequat
+              condimentum velit ut tempor. Nam porta massa in metus bibendum congue. Pellentesque ultrices vestibulum
+              mattis.
+            </p>
+            <p className="text-wrapper-2">
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed non dui sodales, faucibus libero ut, posuere
+              felis. Donec imperdiet suscipit accumsan. Aenean consequat condimentum velit ut tempor. Nam porta massa in
+              metus bibendum congue. Pellentesque ultrices liquam egestas nunc at
+            </p>
+            <p className="text-wrapper-3">Lorem ipsum dolor sit amet</p>
+            <img className="img" alt="Url icon" src="url-icon-1.png" />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
